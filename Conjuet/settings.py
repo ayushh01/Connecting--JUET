29546ac545c81c18c5e1ffa627f9ff1f5ebcc0d5
Ayush Srivastava <srivastavaayush.alld@gmail.com>
2020-05-15 17:48:45 +0530

    added env

--- Conjuet/settings.py
+++ Conjuet/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rv5ihx)9a8j%668=jh50j*&nt!cl0!f+xx5vv-%=ovbv9%$n-z'
-
+#SECRET_KEY = 'rv5ihx)9a8j%668=jh50j*&nt!cl0!f+xx5vv-%=ovbv9%$n-z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
