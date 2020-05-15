e822ad9620d78a93ffcb3bf7263f8cae7c28ab98
Ayush Srivastava <srivastavaayush.alld@gmail.com>
2020-05-15 17:59:46 +0530

    updated

--- Conjuet/settings.py
+++ Conjuet/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +129,4 @@ LOGIN_REDIRECT_URL= 'profile/'
 
 EMAIL_HOST='localhost'
 EMAIL_PORT = 1025
+django_heroku.settings(locals())
