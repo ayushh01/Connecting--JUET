c88ccd9dfb915eb3301cfabe84d0f2f6bac5cf49
Ayush Srivastava <srivastavaayush.alld@gmail.com>
2020-05-15 14:29:53 +0530

    conjuet

--- Conjuet/settings.py
+++ Conjuet/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,12 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media' )
 MEDIA_URL  = '/media/'
 
